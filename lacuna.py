f7debc1c26d87862f03e52b758f33b8bdbf7ee29
anonymous-1234567 <197023679+anonymous-1234567@users.noreply.github.com>
2025-05-15 13:07:27 -0500

    Add files via upload

--- lacuna.py
+++ lacuna.py
@@ -9,9 +9,9 @@ class Lacuna100Large(VisionDataset):
     def __init__(self, root, transform=None):
         super(Lacuna100Large, self).__init__(root, transform=transform)
 
-        self.targets = np.load(os.path.join(self.root, 'label.npy')) #avoid accessing this
+        self.targets = np.load(os.path.join(self.root, 'label.npy')) #avoid accessing this 
         self.identities = np.load(os.path.join(self.root,  'identities.npy')) #avoid accessing this
-        self.indices=np.arange(len(self.targets)) #controls the data we actually have access to
+        self.indices=np.arange(len(self.targets)) #controls the data we actually have access to, indices of individual data samples
 
         # The data is saved in BGR format. Convert to RGB.
         #self.data = self.data[...,::-1]
@@ -47,3 +47,20 @@ class Lacuna100Large(VisionDataset):
     
     def reset(self):
         self.indices = np.arange(len(self.targets))
+        
+    def get_data_by_index(self, index):
+        img = np.load(os.path.join(self.root, f'image{index}.npy'))
+
+        target =  self.targets[index]
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        img = Image.fromarray(img) #PIL image
+        if self.transform is not None:
+            img = self.transform(img)
+
+            
+        identity = self.identities[index]          
+
+        return img, target, identity
